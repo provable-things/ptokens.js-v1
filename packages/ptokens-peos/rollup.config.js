9ba6ceb4dc25cb02dccf3ed5f2e123afd29e18b4
allemanfredi <alle.manfredi@gmail.com>
2019-10-31 15:36:57 +0100

    chore(ptokens-peos): adds ptokens-enclave in rollup build options

--- packages/ptokens-peos/rollup.config.js
+++ packages/ptokens-peos/rollup.config.js
@@ -2,4 +2,6 @@
 import pkg from './package.json'
 import rollupConfig from '../../rollup.config'
 
-export default rollupConfig('pEOS', pkg.name)
+export default rollupConfig('pEOS', pkg.name, {
+  'ptokens-enclave': 'ptokens-enclave'
+})
