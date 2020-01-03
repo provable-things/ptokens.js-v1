3b15fa930fc82a394f18a7c5dac01e92e241c99d
allemanfredi <alle.manfredi@gmail.com>
2020-01-03 18:08:21 +0100

    feat(ptokens-utils): adds converters

---
+++ packages/ptokens-utils/src/converters.js
@@ -0,0 +1,31 @@
+const UINT32_MAX = Math.pow(2, 32)
+
+/**
+ * @param {Buffer} buffer
+ */
+const decodeUint64le = buffer => {
+  const rem = buffer.readUInt32LE(0)
+  const top = buffer.readUInt32LE(4)
+
+  return top * UINT32_MAX + rem
+}
+
+/**
+ * @param {Number} number
+ */
+const encodeUint64le = number => {
+  const buffer = Buffer.alloc(8)
+
+  const top = Math.floor(number / UINT32_MAX)
+  const rem = number - top * UINT32_MAX
+
+  buffer.writeUInt32LE(rem, 0)
+  buffer.writeUInt32LE(top, 4)
+
+  return buffer
+}
+
+module.exports = {
+  decodeUint64le,
+  encodeUint64le
+}
--- packages/ptokens-utils/src/index.js
+++ packages/ptokens-utils/src/index.js
@@ -1,8 +1,10 @@
+import * as converters from './converters'
 import * as eth from './eth'
 import * as eos from './eos'
 import * as helpers from './helpers'
 
 export default {
+  converters,
   eos,
   eth,
   helpers
