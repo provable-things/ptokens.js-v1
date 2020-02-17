b13230c11464ae9dbbfe6c23352e97a0c5a02922
allemanfredi <alle.manfredi@gmail.com>
2020-02-17 18:49:27 +0100

    chore(ptokens-node): <- adds types for this

---
+++ packages/ptokens-node/types/index.d.ts
@@ -0,0 +1,63 @@
+import { NodeSelector } from 'ptokens-node-selector'
+import { EventEmitter } from 'events'
+
+export class Node extends NodeSelector {
+  constructor(
+    configs: {
+      pToken: {
+        name: string,
+        redeemFrom: string
+      },
+      defaultNode?: string
+    }
+  )
+
+  ping(): Promise<string>
+
+  getInfo(_issueFromNetwork: string, _redeemFromNetwork: string): Promise<NodeInfo>
+
+  getReports(_type: string, _limit?: number): Promise<ReportList>
+
+  getReportsByAddress(_type: string, _address: string, _limit?: number): Promise<ReportList>
+
+  getReportByNonce(_type: string, _nonce: number): Promise<Report>
+
+  getLastProcessedBlock(_type: string): Promise<number>
+
+  getIncomingTransactionStatus(_hash: string): Promise<Report>
+
+  getBroadcastTransactionStatus(_hash: string): Promise<Report>
+
+  submitBlock(_type: string, _block: object): Promise<string>
+
+  monitorIncomingTransaction(_hash: string, _type: string, _eventEmitter: EventEmitter): Promise<Report>
+}
+
+export interface NodeInfo {
+  "public-key": string,
+  "smart-contract-address": string
+}
+
+export interface ReportList extends Array<Report> {}
+
+export interface Report {
+  _id: string,
+  broadcast: true,
+  broadcast_tx_hash: string,
+  broadcast_timestamp: number,
+  native_tx_hash?: string,
+  host_tx_hash?: string,
+  native_signed_tx?: string,
+  host_signed_tx?: string,
+  native_tx_amount?: number,
+  host_tx_amount?: number,
+  native_tx_recipient?: string,
+  host_tx_recipient?: string,
+  native_account_nonce: number,
+  host_account_nonce: number,
+  originating_tx_hash: string,
+  originating_address: string,
+  witnessed_timestamp: number,
+  native_latest_block_number?: number,
+  host_latest_block_number?: number
+}
---
+++ packages/ptokens-node/types/tests/ptokens-node-test.ts
@@ -0,0 +1,62 @@
+import { Node } from 'ptokens-node'
+import { EventEmitter } from 'events'
+
+const HASH_INCOMING_TX =
+  'a177f86e24eb3ffc0a272f7f0bd6cb8fb6acb97a67ac211a7863b12dfcec1a29'
+const HASH_BROADCASTED_TX =
+  '0xac53ba6214ad2b0513fd6d69ab2c39a6649fc83a61048eb5d4aebad80f0cbe30'
+
+const BTC_TESTING_ADDRESS = '2NFLTr9nFbnexQgRP3hpEH5NKduvqpiAUpw'
+
+const BTC_PBTC_BLOCK = {
+  id: '00000000000013068675dc3694319815bef985de15deb505541d70387a193e69',
+  height: 1657098,
+  version: 536928256,
+  timestamp: 1578591247,
+  tx_count: 5,
+  size: 1297,
+  weight: 4099,
+  merkle_root:
+    'd7e2095d95a0174bbe4503181b80d5d4136d088123e4057401f5ca7bdcc36718',
+  previousblockhash:
+    '00000000000068cfcbcc8023d97b0900d6c905f2dbf5fffef34c6b7dfb07d254',
+  nonce: 729798261,
+  bits: 453023995
+}
+
+const node = new Node({
+  pToken: {
+    name: 'pBTC',
+    redeemFrom: 'ETH'
+  }
+})
+
+// $ExpectType Promise<string>
+node.ping()
+
+// $ExpectType Promise<NodeInfo>
+node.getInfo('testnet', 'ropsten')
+
+// $ExpectType Promise<ReportList>
+node.getReports('native', 10)
+
+// $ExpectType Promise<ReportList>
+node.getReportsByAddress('native', BTC_TESTING_ADDRESS, 10)
+
+// $ExpectType Promise<Report>
+node.getReportByNonce('native', 1)
+
+// $ExpectType Promise<number>
+node.getLastProcessedBlock('native')
+
+// $ExpectType Promise<Report>
+node.getIncomingTransactionStatus(HASH_INCOMING_TX)
+
+// $ExpectType Promise<Report>
+node.getBroadcastTransactionStatus(HASH_BROADCASTED_TX)
+
+// $ExpectType Promise<string>
+node.submitBlock('native', BTC_PBTC_BLOCK)
+
+// $ExpectType Promise<Report>
+node.monitorIncomingTransaction(HASH_INCOMING_TX, 'native', new EventEmitter())
