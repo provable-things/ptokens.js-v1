526419b80ba3d4270c43e6f5cacc77f786e7a13f
allemanfredi <alle.manfredi@gmail.com>
2020-02-18 09:19:23 +0100

    chore(ptokens-utils): change param name in converters

--- packages/ptokens-utils/src/converters.js
+++ packages/ptokens-utils/src/converters.js
@@ -1,23 +1,23 @@
 const UINT32_MAX = Math.pow(2, 32)
 
 /**
- * @param {Buffer} buffer
+ * @param {Buffer} _buffer
  */
-const decodeUint64le = buffer => {
-  const rem = buffer.readUInt32LE(0)
-  const top = buffer.readUInt32LE(4)
+const decodeUint64le = _buffer => {
+  const rem = _buffer.readUInt32LE(0)
+  const top = _buffer.readUInt32LE(4)
 
   return top * UINT32_MAX + rem
 }
 
 /**
- * @param {Number} number
+ * @param {Number} _number
  */
-const encodeUint64le = number => {
+const encodeUint64le = _number => {
   const buffer = Buffer.alloc(8)
 
-  const top = Math.floor(number / UINT32_MAX)
-  const rem = number - top * UINT32_MAX
+  const top = Math.floor(_number / UINT32_MAX)
+  const rem = _number - top * UINT32_MAX
 
   buffer.writeUInt32LE(rem, 0)
   buffer.writeUInt32LE(top, 4)
