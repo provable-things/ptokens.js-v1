692826bd00f769f54bf9b3b66fd2eab6af8696f4
allemanfredi <alle.manfredi@gmail.com>
2020-04-29 10:46:25 +0200

    chore(ptokens-node-selector): adds constants within tests

--- packages/ptokens-node-selector/tests/ptokens-node-selector.test.js
+++ packages/ptokens-node-selector/tests/ptokens-node-selector.test.js
@@ -1,13 +1,14 @@
 import { NodeSelector } from '../src/index'
 import { expect } from 'chai'
+import { constants } from 'ptokens-utils'
 
 jest.setTimeout(300000)
 
 test('Should select a pBTC node on EOS Jungle3 Testnet', async () => {
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'EOS',
-    network: 'testnet'
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Eosio,
+    network: constants.networks.Testnet
   })
 
   const node = await nodeSelector.select()
@@ -21,9 +22,9 @@ test('Should select a pBTC node on EOS Jungle3 Testnet', async () => {
 
 test('Should select a pBTC node on EOS Mainnet', async () => {
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'EOS',
-    network: 'mainnet'
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Eosio,
+    network: constants.networks.Mainnet
   })
 
   const node = await nodeSelector.select()
@@ -37,9 +38,9 @@ test('Should select a pBTC node on EOS Mainnet', async () => {
 
 test('Should select a pBTC node on EOS Jungle3 Testnet with detailed initialization', async () => {
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    hostBlockchain: 'EOSIO',
-    hostNetwork: 'testnet_jungle3'
+    pToken: constants.pTokens.pBTC,
+    hostBlockchain: constants.blockchains.Eosio,
+    hostNetwork: constants.networks.EosioJungle3
   })
 
   const node = await nodeSelector.select()
@@ -53,8 +54,8 @@ test('Should select a pBTC node on EOS Jungle3 Testnet with detailed initializat
 
 test('Should select a pBTC node on EOS Mainnet with detailed initialization', async () => {
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    hostBlockchain: 'EOSIO',
+    pToken: constants.pTokens.pBTC,
+    hostBlockchain: constants.blockchains.Eosio,
     hostNetwork: 'mainnet'
   })
 
@@ -69,9 +70,9 @@ test('Should select a pBTC node on EOS Mainnet with detailed initialization', as
 
 test('Should select a pBTC node on Ethereum Mainnet', async () => {
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'ETH',
-    network: 'mainnet'
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Ethereum,
+    network: constants.networks.Mainnet
   })
 
   const node = await nodeSelector.select()
@@ -85,9 +86,9 @@ test('Should select a pBTC node on Ethereum Mainnet', async () => {
 
 test('Should select a pBTC node on Ethereum Ropsten Testnet', async () => {
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'ETH',
-    network: 'testnet'
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Ethereum,
+    network: constants.networks.Testnet
   })
 
   const node = await nodeSelector.select()
@@ -102,9 +103,9 @@ test('Should select a pBTC node on Ethereum Ropsten Testnet', async () => {
 test('Should not be connected if a node is unreachable on Ethereum', async () => {
   const unreachableNode = 'https://unreachable-node.io'
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'ETH',
-    network: 'testnet'
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Ethereum,
+    network: constants.networks.Testnet
   })
 
   const isConnected = await nodeSelector.checkConnection(unreachableNode, 2000)
@@ -114,9 +115,9 @@ test('Should not be connected if a node is unreachable on Ethereum', async () =>
 test('Should not be connected if a node is unreachable on EOS', async () => {
   const unreachableNode = 'https://unreachable-node.io'
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'EOS',
-    network: 'testnet'
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Eosio,
+    network: constants.networks.Testnet
   })
 
   const isConnected = await nodeSelector.checkConnection(unreachableNode, 2000)
@@ -130,8 +131,8 @@ test('Should generate an error when an invalid pToken name is set', () => {
     // eslint-disable-next-line
     new NodeSelector({
       pToken: 'pBTCInvalid',
-      blockchain: 'EOS',
-      network: 'testnet'
+      blockchain: constants.blockchains.Eosio,
+      network: constants.networks.Testnet
     })
   } catch (err) {
     expect(err.message).to.be.equal(expectedError)
@@ -142,9 +143,9 @@ test('Should generate an error when an invalid node is set', async () => {
   const expectedError = 'Node not found or Wrong Feature'
   const unreachableNode = 'https://unreachable-node.io'
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'ETH',
-    network: 'testnet'
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Ethereum,
+    network: constants.networks.Testnet
   })
 
   try {
@@ -158,9 +159,9 @@ test('Should not select a different node on Ethereum Testnet when a valid one is
   const reachableNode = 'https://nuc-bridge-3.ngrok.io'
 
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'ETH',
-    network: 'testnet',
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Ethereum,
+    network: constants.networks.Testnet,
     defaultEndoint: reachableNode
   })
   const selectedNode = await nodeSelector.select()
@@ -171,9 +172,9 @@ test('Should not select a different node on Ethereum Mainnet when a valid one is
   const reachableNode = 'https://pbtc-node-1a.ngrok.io'
 
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'ETH',
-    network: 'mainnet',
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Ethereum,
+    network: constants.networks.Mainnet,
     defaultEndoint: reachableNode
   })
   const selectedNode = await nodeSelector.select()
@@ -185,9 +186,9 @@ test('Should select a different node on Ethereum Ropsten Testnet when a valid on
     'https://nuc-bridge-2.ngrok.io'
 
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'ETH',
-    network: 'testnet',
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Ethereum,
+    network: constants.networks.Testnet,
     defaultNode: reachableNodeButNotCompatibleWithSelectedpToken
   })
   const selectedNode = await nodeSelector.select()
@@ -201,9 +202,9 @@ test('Should select a different node on EOS Testnet when a valid one is set as d
     'https://nuc-bridge-2.ngrok.io'
 
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'EOS',
-    network: 'testnet',
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Eosio,
+    network: constants.networks.Testnet,
     defaultNode: reachableNodeButNotCompatibleWithSelectedpToken
   })
   const selectedNode = await nodeSelector.select()
@@ -217,9 +218,9 @@ test('Should select a different node on Ethereum Mainnet when a valid one is set
     'https://nuc-bridge-3.ngrok.io'
 
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'ETH',
-    network: 'mainnet',
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Ethereum,
+    network: constants.networks.Mainnet,
     defaultNode: reachableNodeButNotCompatibleWithSelectedpToken
   })
   const selectedNode = await nodeSelector.select()
@@ -232,9 +233,9 @@ test('Should select a different node on Ethereum Testnet when an invalid one is 
   const unreachableNode = 'https://unreachable-node.io'
 
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'ETH',
-    network: 'testnet',
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Ethereum,
+    network: constants.networks.Testnet,
     defaultNode: unreachableNode
   })
   const selectedNode = await nodeSelector.select()
@@ -245,9 +246,9 @@ test('Should select a different node on EOS Testnet when an invalid one is set a
   const unreachableNode = 'https://unreachable-node.io'
 
   const nodeSelector = new NodeSelector({
-    pToken: 'pBTC',
-    blockchain: 'EOS',
-    network: 'testnet',
+    pToken: constants.pTokens.pBTC,
+    blockchain: constants.blockchains.Eosio,
+    network: constants.networks.Testnet,
     defaultNode: unreachableNode
   })
   const selectedNode = await nodeSelector.select()
